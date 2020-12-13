509d4a2547d43a71508937ddba147e0b2c7d0695
Sukhraj Rattan <sukhraj.rattan@tesco.com>
2020-12-13 15:34:53 +0000

    added some tests for indirect relationship

--- src/test/java/day7/BagSolutionTest.java
+++ src/test/java/day7/BagSolutionTest.java
@@ -46,5 +46,34 @@ public class BagSolutionTest {
 
     }
 
+    @Test
+    public void testBagWithMultipleTargetBagsOneIsIndirect() {
+
+        testInput = "dark orange bags contain 3 bright white bags, 4 muted yellow bags.\n" +
+            "muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.";
+
+
+        BagSolution bagSolution = new BagSolution(testInput);
+        int result = bagSolution.solution("shiny gold bag");
+
+        assertEquals(1, result);
+
+    }
+
+    @Test
+    public void testBagWithMultipleTargetBagsTwoAreIndirect() {
+
+        testInput = "dark orange bags contain 3 bright white bags, 4 muted yellow bags.\n" +
+            "muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.\n" +
+            "bright blue bags contain 2 black bags.\n" +
+            "black bags contain 1 shiny gold bag.";
+
+
+        BagSolution bagSolution = new BagSolution(testInput);
+        int result = bagSolution.solution("shiny gold bag");
+
+        assertEquals(2, result);
+
+    }
 
 }
